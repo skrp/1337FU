c6258a736cc688afb7d7e6a5f9ead028a20886ef
Prince of Archives <rich.tijerina@gmail.com>
2018-07-28 22:16:21 -0700

    Update LONG.c

--- LONG.c
+++ LONG.c
@@ -1,4 +1,7 @@
 // LONG - c long form ////////////////////
 #include <stdio.h>
-#define MX 10
+#define MAX 10
+#define RED "\x1b[31m"
+#define RESET "\x1b[0m"
+#define GREEN "\x1b[32m"
 //////////////////////////////////////////
